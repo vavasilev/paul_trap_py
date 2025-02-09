ebf9687794529c22aac69139d1bfe65cc736774e
I572491 <vasil.vasilev04@sap.com>
2025-02-09 15:25:39 +0200

    calc state

--- app/main.py
+++ app/main.py
@@ -5,14 +5,33 @@ from app.evolution_calculator import EvolutionCalculator
 
 
 def main():
-    find_couplings()
+    calculate_reflections()
+
+
+def calculate_reflections():
+    couplings: np.ndarray = np.array([1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0])
+
+    A_S: np.ndarray
+    A_U: np.ndarray
+
+    red_blue_split = np.split(couplings, 2)
+    red_couplings: np.ndarray = red_blue_split[0]
+    blue_couplings: np.ndarray = red_blue_split[1]
+    calc: EvolutionCalculator = EvolutionCalculator(int(len(couplings) / 2))
+    v_matrix: np.ndarray = calc.create_v_matrix(red_couplings, blue_couplings)
+    A_U, A_S, B_U, B_S = calc.calculate_ms_values_and_vectors(v_matrix)
+
+    display_vector(A_S)
+    display_matrix(A_U)
 
 
 def find_couplings():
-    desired_array: np.ndarray = [0, 0.5, 0.5, 0, 0.5, 0, 0, 0.5]
+    desired_array: np.ndarray = [0, 0, 0, 0, 0, 0, 0, 1]
+    #desired_array: np.ndarray = [0, 0, 0, -0.544, 0, 0.375, 0.375, 0, 0, 0.375, 0.375, 0, 0.375, 0, 0, 0]
+    #desired_array: np.ndarray = [0, 0, 0, 0.408, 0, 0.408, 0.408, 0, 0, 0.408, 0.408, 0, 0.408, 0, 0, 0]
     ion_num: int = 3
     is_complex: bool = False
-    accuracy: float = 10e-4
+    accuracy: float = 10e-5
     coupling_range: int = 10
 
     min_distance: float = 100.0
